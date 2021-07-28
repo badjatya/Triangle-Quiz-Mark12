8d78b39b1c291cad8daaf625d5c25dd308585e99
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-28 06:10:52 +0530

    Style: Navbar styled

--- src/App/Components/Navbar/Navbar.js
+++ src/App/Components/Navbar/Navbar.js
@@ -9,16 +9,50 @@ import Colors from "./../../Constants/Colors";
 const NavbarContainer = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  width: 1024px;
+  max-width: 1024px;
+  margin: 10px auto;
 `;
 
 const Logo = styled.p`
   color: ${Colors.primary};
+  font-weight: 700;
+  font-size: 1.2rem;
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const NavItem = styled.p`
+  display: block;
+  text-decoration: none;
+  color: ${Colors.primary};
+  padding-bottom: 10px;
+  cursor: pointer;
+  transition: all 0.3s ease;
+
+  &:not(:last-child) {
+    margin-right: 35px;
+  }
+
+  &:hover {
+    border-bottom: 3px solid ${Colors.primary};
+  }
 `;
 
 const Navbar = () => {
   return (
     <NavbarContainer>
-      <Logo>nav</Logo>
+      <Logo>TriangleQuiz</Logo>
+
+      <Nav>
+        <NavItem>Area</NavItem>
+        <NavItem>Perimeter</NavItem>
+      </Nav>
     </NavbarContainer>
   );
 };
