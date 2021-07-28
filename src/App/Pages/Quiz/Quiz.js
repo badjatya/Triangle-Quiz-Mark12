94ad6855a0f654ee689d7614e5b05705dd48163f
Archit Badjatya <56677912+badjatya@users.noreply.github.com>
2021-07-28 13:05:06 +0530

    Quiz Completed

--- src/App/Pages/Quiz/Quiz.js
+++ src/App/Pages/Quiz/Quiz.js
@@ -1,7 +1,120 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Importing Styled Components
+import styled from "styled-components";
+
+// Importing Components
+import CustomButton from "../../Components/CustomButton/CustomButton";
+
+// Importing Data
+import questions from "../../data/question.data";
+
+// Importing Colors
+import Colors from "../../Constants/Colors";
+
+const Main = styled.main`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const QuizContainer = styled.div`
+  width: 600px;
+  margin-top: 100px;
+  background-color: ${Colors.white};
+  padding: 30px 50px;
+`;
+
+const QuizDetail = styled.div`
+  display: flex;
+`;
+
+const QuestionIndex = styled.p`
+  margin-right: 15px;
+  width: 20%;
+`;
+
+const QuestionContainer = styled.div`
+  width: 80%;
+`;
+
+const Option = styled.p`
+  border: 1px solid ${Colors.primary};
+  padding: 0.5rem 1rem;
+  color: ${Colors.primary};
+  border-radius: 30px;
+  cursor: pointer;
+`;
+
+const ResultContainer = styled.div``;
+
+const Result = styled.p`
+  border: 1px solid ${Colors.primary};
+  padding: 1rem 0;
+  color: ${Colors.primary};
+  border-radius: 30px;
+  text-align: center;
+`;
 
 const Quiz = () => {
-  return <div></div>;
+  //State
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+
+  // Handling Answers
+  const handleAnswerOptionClick = (isCorrect) => {
+    if (isCorrect) {
+      setScore(score + 1);
+    }
+
+    const nextQuestion = currentQuestion + 1;
+    if (nextQuestion < questions.length) {
+      setCurrentQuestion(nextQuestion);
+    } else {
+      setShowScore(true);
+    }
+  };
+
+  // Reset
+  const resetHandler = () => {
+    setScore(0);
+    setShowScore(false);
+    setCurrentQuestion(0);
+  };
+
+  return (
+    <Main>
+      <QuizContainer>
+        {showScore ? (
+          <ResultContainer>
+            <Result>
+              You scored {score} out of {questions.length}
+            </Result>
+            <CustomButton onClick={resetHandler} style={{ width: "100%" }}>
+              Play Again
+            </CustomButton>
+          </ResultContainer>
+        ) : (
+          <QuizDetail>
+            <QuestionIndex>Question {currentQuestion + 1}</QuestionIndex>
+            <QuestionContainer>
+              <p>{questions[currentQuestion].questionText}</p>
+              {questions[currentQuestion].answerOptions.map((answerOption) => (
+                <Option
+                  onClick={() =>
+                    handleAnswerOptionClick(answerOption.isCorrect)
+                  }
+                >
+                  {answerOption.answerText}
+                </Option>
+              ))}
+            </QuestionContainer>
+          </QuizDetail>
+        )}
+      </QuizContainer>
+    </Main>
+  );
 };
 
 export default Quiz;
---
+++ src/App/data/question.data.js
@@ -0,0 +1,51 @@
+const questions = [
+  {
+    questionText:
+      "A ________ triangle is one where all 3 angles measure less than 90 degrees?",
+    answerOptions: [
+      { answerText: "A. Acute Triangle", isCorrect: true },
+      { answerText: "B. Obtuse Triangle", isCorrect: false },
+      { answerText: "C. Right Triangle", isCorrect: false },
+      { answerText: "D. Equilateral Triangle", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "A triangle where all sides are congruent is called ?",
+    answerOptions: [
+      { answerText: "A. Scalene Triangle", isCorrect: false },
+      { answerText: "B. Isosceles Triangle", isCorrect: false },
+      { answerText: "C. Equilateral Triangle", isCorrect: true },
+      { answerText: "D. Right Triangle", isCorrect: false },
+    ],
+  },
+  {
+    questionText:
+      "A triangle with no congruent sides and 1 angle greater than 90 degrees would be called __________ _________",
+    answerOptions: [
+      { answerText: "A. Scalene Triangle", isCorrect: false },
+      { answerText: "B. Isosceles Triangle", isCorrect: false },
+      { answerText: "C. Obtuse Scalene Triangle", isCorrect: true },
+      { answerText: "D. Right Triangle", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "All 3 interior angles of a triangle add up to?",
+    answerOptions: [
+      { answerText: "A. 45 degrees", isCorrect: false },
+      { answerText: "B. 270 degrees", isCorrect: false },
+      { answerText: "C. 90 degrees", isCorrect: false },
+      { answerText: "D. 180 degrees", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "Which type of triangle has one 90 degree angle.?",
+    answerOptions: [
+      { answerText: "A. Scalene Triangle", isCorrect: false },
+      { answerText: "B. Right Triangle", isCorrect: true },
+      { answerText: "C. Obtuse Scalene Triangle", isCorrect: false },
+      { answerText: "D. Isosceles Triangle", isCorrect: false },
+    ],
+  },
+];
+
+export default questions;
